d8df89cc1166cdcb25fca8c6f7b9cee4607dc86b
evaldocunhaf <evaldocunhaf@gmail.com>
2023-09-25 18:55:45 -0300

    ajuste geral do projeto devido a problema com libs

--- PKolekto/Kolekto/models.py
+++ PKolekto/Kolekto/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from cpf_field.models import CPFField
-from cep.models import Cep
 
 # Create your models here.
+
+
 class Loja(models.Model):
     Banner = models.ImageField(upload_to="ban_imgs/")
     Perfil = models.ImageField(upload_to="perf_imgs/")
     NomeLoja = models.CharField(max_length=30)
     NomeVendedor = models.CharField(max_length=50)
-    Cpf = CPFField("cpf")
+    Cpf = models.IntegerField()
     DataNascimento = models.DateTimeField()
-    #Localizacao = Cep()
+    Localizacao = models.TextField(max_length=50)
--- PKolekto/PKolekto/urls.py
+++ PKolekto/PKolekto/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for PKolekto project.
+"""PKolekto URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("Kolekto/", include("Kolekto.urls")),
+    path("Kolekto/", include("Kolekto.urls"))
 ]
