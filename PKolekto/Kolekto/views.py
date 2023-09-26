dd6b9fd3abc2ee11084dc9c08104f93038274d7c
evaldocunhaf <evaldocunhaf@gmail.com>
2023-09-26 20:51:13 -0300

    ajuste do forms de criação de loja

--- PKolekto/Kolekto/views.py
+++ PKolekto/Kolekto/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
-from .models import Produto
+from .models import Produto, Loja
 # Create your views here.
 
+'''def val_cpf(num):
+    num = num.split('.')
+    aux = num[2].split('-')
+    num.append(aux[0])
+    num.append(aux[1])
+    num'''
 def Cadastro_Loja(request):
-    if request.method == "POST":
-        cidade = request.POST.get("cidade", None)
-        print(cidade)
-
     contexto = {
         "nome_vendedor": "Marcílio"
     }
+    if request.method == "POST":
+        data_nascimento = request.POST.get("nascimento")
+        Localizacao = f"{request.POST.get('cidade')}, {request.POST.get('estado')}"
+        cpf = request.POST.get(("cpf"))
+        nome_loja = request.POST.get("nome_loja")
+        banner = request.POST.get("banner")
+        perfil = request.POST.get("perfil")
+        nome_vendedor = contexto["nome_vendedor"]
+        Loja.objects.create(Banner=banner, Perfil=perfil, NomeLoja=nome_loja, NomeVendedor=nome_vendedor, Cpf=cpf,
+                            DataNascimento=data_nascimento, Localizacao=Localizacao)
+
+
     return render(request, "cadastro_loja.html", context=contexto)
+
 
 def Add_Produto(request):
     if request.method == "POST":
