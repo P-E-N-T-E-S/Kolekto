132b617e06f999e65191de7c198268e0fed70704
evaldocunhaf <evaldocunhaf@gmail.com>
2023-09-26 08:19:53 -0300

    ajuste na view do cadastro e no model de produto

--- PKolekto/Kolekto/admin.py
+++ PKolekto/Kolekto/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Loja
+from .models import Loja, Produto
 
 # Register your models here.
 
 admin.site.register(Loja)
+admin.site.register(Produto)
