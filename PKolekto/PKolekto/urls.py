db093000025a146633d59d7235e59f4a050567b5
Luiz-Edu0202 <brayner.luizeduardo@gmail.com>
2023-10-05 12:04:36 -0300

    A url vazia vai para Home close #25

--- PKolekto/PKolekto/urls.py
+++ PKolekto/PKolekto/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("Kolekto/", include("Kolekto.urls"))
+    path("Kolekto/", include("Kolekto.urls")),
+    path("",include("Kolekto.urls"))
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
