0eb9b93dc9bab5209330c94b96ddc91dc2da488d
evaldocunhaf <evaldocunhaf@gmail.com>
2023-10-27 18:19:41 -0300

    ajustes nos testes, todos até agora funcionais

--- PKolekto/Kolekto/tests2.py
+++ PKolekto/Kolekto/tests2.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 import time
 
 driver = webdriver.Chrome()
@@ -18,7 +19,7 @@ class Historia2(LiveServerTestCase):
 
             usuario.send_keys(f"Teste2{i}")
             nome_usuario.send_keys(f"Marcílio{i}")
-            email.send_keys(f"teste{i}@teste.com")
+            email.send_keys(f"hit2{i}@teste.com")
             senha.send_keys("Teste12345")
             time.sleep(2)
             botao.send_keys(Keys.ENTER)
@@ -142,9 +143,10 @@ class Historia2(LiveServerTestCase):
             "Login"
         )
 
+
 class Historia3(LiveServerTestCase):
     def test_000_setup(self):
-        for i in range(4):
+        for i in range(2):
             driver.get("http://127.0.0.1:8000/registro")
             usuario = driver.find_element(by=By.NAME, value="username")
             nome_usuario = driver.find_element(by=By.NAME, value="nome")
@@ -154,12 +156,12 @@ class Historia3(LiveServerTestCase):
 
             usuario.send_keys(f"Teste3{i}")
             nome_usuario.send_keys(f"Marcílio{i}")
-            email.send_keys(f"teste{i}@teste.com")
+            email.send_keys(f"hist3{i}@teste.com")
             senha.send_keys("Teste12345")
             time.sleep(2)
             botao.send_keys(Keys.ENTER)
-
-            if i == 0:
+            if i == 1:
+                time.sleep(2)
                 driver.get("http://127.0.0.1:8000/nova_loja")
                 nascimento = driver.find_element(by=By.NAME, value="nascimento")
                 cidade = driver.find_element(by=By.NAME, value="cidade")
@@ -171,63 +173,73 @@ class Historia3(LiveServerTestCase):
                 time.sleep(2)
                 enviar = driver.find_element(by=By.NAME, value="criar")
 
-                nascimento.send_keys("24112004")
-                cidade.send_keys("Goiana")
+                nascimento.send_keys("29082003")
+                cidade.send_keys("Rio Branco")
                 cpf.send_keys("000.000.000-00")
-                nome_loja.send_keys("Recicards")
-                imgperfil.send_keys("https://i.imgur.com/zdftIp6.jpeg")
-                imgbanner.send_keys("https://i.imgur.com/dcGl7yo.png")
+                nome_loja.send_keys("Lojateste3")
+                imgperfil.send_keys("https://i.imgur.com/FWUCFTF.jpeg")
+                imgbanner.send_keys("https://i.imgur.com/T2umQUo.jpeg")
                 descloja.send_keys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
                 time.sleep(2)
                 enviar.send_keys(Keys.ENTER)
+                nomes = ['Charizard 1999 - 1° Edição', 'Black Lotus - Beta']
 
-                driver.get("http://127.0.0.1:8000/add_produto")
-                foto = driver.find_element(by=By.NAME, value="foto1")
-                prod = driver.find_element(by=By.NAME, value="nome_produto")
-                descricao = driver.find_element(by=By.NAME, value="descricao")
-                preco = driver.find_element(by=By.NAME, value="preco")
-                qntd = driver.find_element(by=By.NAME, value="qntd")
-                categoria = driver.find_element(by=By.NAME, value="select")
-                enviar = driver.find_element(by=By.NAME, value="Add")
+                for r in range(2):
+                    time.sleep(2)
+                    driver.get("http://127.0.0.1:8000/add_produto")
+                    foto = driver.find_element(by=By.NAME, value="foto1")
+                    prod = driver.find_element(by=By.NAME, value="nome_produto")
+                    descricao = driver.find_element(by=By.NAME, value="descricao")
+                    preco = driver.find_element(by=By.NAME, value="preco")
+                    qntd = driver.find_element(by=By.NAME, value="qntd")
+                    categoria = driver.find_element(by=By.NAME, value="select")
+                    categoria = Select(categoria)
+                    enviar = driver.find_element(by=By.NAME, value="Add")
 
-                foto.send_keys("https://static22.minhalojanouol.com.br/brofistloja/produto/20210701161644_6137993863_D.jpg")
-                prod.send_keys("Charizard 1999 - 1° Edição")
-                descricao.send_keys("Carta Charizard 1999 - 1° Edição")
-                preco.send_keys("1700000")
-                qntd.send_keys("1")
-                categoria.select_by_visible_text("Cartas")
-                time.sleep(2)
-                enviar.send_keys(Keys.ENTER)
-
-                driver.get("http://127.0.0.1:8000/add_produto")
-                foto = driver.find_element(by=By.NAME, value="foto1")
-                prod = driver.find_element(by=By.NAME, value="nome_produto")
-                descricao = driver.find_element(by=By.NAME, value="descricao")
-                preco = driver.find_element(by=By.NAME, value="preco")
-                qntd = driver.find_element(by=By.NAME, value="qntd")
-                enviar = driver.find_element(by=By.NAME, value="Add")
-
-                foto.send_keys("https://i.ebayimg.com/images/g/HbYAAOSwKeVjOsBa/s-l1600.jpg")
-                prod.send_keys("Black Lotus - Beta")
-                descricao.send_keys("Black Lotus - Beta, uma das cartas mais raras do Magic")
-                preco.send_keys("4039553")
-                qntd.send_keys("1")
-                time.sleep(2)
-                enviar.send_keys(Keys.ENTER)
-
+                    foto.send_keys("https://i.ebayimg.com/images/g/HbYAAOSwKeVjOsBa/s-l1600.jpg")
+                    prod.send_keys(nomes[r])
+                    descricao.send_keys("Black Lotus - Beta, uma das cartas mais raras do Magic")
+                    preco.send_keys("4039553")
+                    qntd.send_keys("1")
+                    time.sleep(2)
+                    categoria.select_by_visible_text("Cartas")
+                    time.sleep(2)
+                    enviar.send_keys(Keys.ENTER)
         self.assertTrue(
             True
         )
         driver.get("http://127.0.0.1:8000/logout/")
 
+    def test_001_cenario1(self):
+        driver.get("http://127.0.0.1:8000/login")
 
-    #def test_003_cenario1(self):
+        usuario = driver.find_element(by=By.NAME, value="username")
+        senha = driver.find_element(by=By.NAME, value="senha")
+        enviar = driver.find_element(by=By.NAME, value="Logar")
 
-    #def test_003_cenario2(self):
-      
-    #def teste_003_cenario3(self):
+        usuario.send_keys("Teste30")
+        senha.send_keys("Teste12345")
+        time.sleep(2)
+        enviar.send_keys(Keys.ENTER)
+        time.sleep(2)
 
-    #def teste_003_cenario4(self):
+        div = driver.find_element(by=By.NAME, value="Charizard 1999 - 1° Edição")
+        idproduto = div.find_element(by=By.TAG_NAME, value="a").get_attribute("name")
+
+        driver.get(f"http://127.0.0.1:8000/Produto/{idproduto}")
+        time.sleep(2)
+
+        favorito = driver.find_element(By.ID, value="adicionarListaDesejos")
+
+        favorito.click()
+        time.sleep(2)
+
+        driver.get("http://127.0.0.1:8000/lista_desejos")
+
+        self.assertEquals(
+            driver.find_element(by=By.ID, value="Charizard 1999 - 1° Edição").text,
+            "Charizard 1999 - 1° Edição"
+        )
 
 
 class Historia4(LiveServerTestCase):
@@ -243,7 +255,7 @@ class Historia4(LiveServerTestCase):
 
             usuario.send_keys(f"Teste4{i}")
             nome_usuario.send_keys(f"João{i}")
-            email.send_keys(f"teste{i}@teste.com")
+            email.send_keys(f"hist4{i}@teste.com")
             senha.send_keys("Teste12345")
             botao.send_keys(Keys.ENTER)
 
@@ -360,5 +372,5 @@ class Historia4(LiveServerTestCase):
             "Login"
         )
 
-    '''def teste_003_cenario4(self):
-        driver.get("http://127.0.0.1:8000/login")'''
+    #def teste_003_cenario4(self):
+        #driver.get("http://127.0.0.1:8000/login")
