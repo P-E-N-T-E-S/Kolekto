c1c4f2608958bd437e718700623045251e3f492c
Luiz Eduardo Araújo Brayner <Brayner.luizeduardo@Gmail.com>
2023-11-26 16:47:46 -0300

    resolver problema dos teste no deploy

--- PKolekto/Kolekto/tests/selenium_setup.py
+++ PKolekto/Kolekto/tests/selenium_setup.py
@@ -1,27 +1,11 @@
 from selenium import webdriver
-import chromedriver_autoinstaller
-
-
 
 driver = None
-chrome_options = webdriver.ChromeOptions()
-
-options = [
-    "--ignore-certificate-errors",
-    "--window-size=1920,1200",
-    "--headless",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-]
-
-for option in options:
-    chrome_options.add_argument(option)
 
 def setup_selenium():
     global driver
-    chromedriver_autoinstaller.install()
     if driver is None:
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome()
     return driver
 
 def finalizar_selenium():
