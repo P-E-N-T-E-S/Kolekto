2b217e66ca794b25bada5bec0fc41be5bd078807
evaldocunhaf <evaldocunhaf@gmail.com>
2023-11-26 21:48:07 -0300

    ajuste nos testes para deploy

--- PKolekto/Kolekto/tests/test_historia05.py
+++ PKolekto/Kolekto/tests/test_historia05.py
@@ -11,7 +11,7 @@ segundos = 0
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--disable-browser-side-navigation")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("window-size=1440,1080")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--disable-extensions")
