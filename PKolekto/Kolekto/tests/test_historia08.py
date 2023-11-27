ea34988d52fbec6a4af0f6c5f1ec0dc0d5cb31be
evaldocunhaf <evaldocunhaf@gmail.com>
2023-11-26 21:52:32 -0300

    ajustes nos testes para deploy

--- PKolekto/Kolekto/tests/test_historia08.py
+++ PKolekto/Kolekto/tests/test_historia08.py
@@ -9,13 +9,7 @@ import time
 segundos = 0
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-browser-side-navigation")
-chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument("--headless")
-chrome_options.add_argument("window-size=1440,1080")
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument("--disable-extensions")
-
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 
