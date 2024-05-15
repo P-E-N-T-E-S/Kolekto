22f556fbe717aa4fd0cfce9540d347ef4fc8498e
Evaldo Cunha F <evaldocunhaf@gmail.com>
2024-05-15 14:44:11 -0300

    Update test.py

--- PKolekto/Kolekto/test.py
+++ PKolekto/Kolekto/test.py
@@ -7,7 +7,13 @@ import time
 
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-browser-side-navigation")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("window-size=1440,1080")
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-extensions")
+
 
 driver = webdriver.Chrome(options=chrome_options)
 
